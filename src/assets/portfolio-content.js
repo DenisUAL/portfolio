facf201a10d20cb067c7af5d881f160e5b0591a3
DenisUAL <denisandreev64@gmail.com>
2017-11-26 14:30:09 -0500

    photos changed

--- src/assets/portfolio-content.js
+++ src/assets/portfolio-content.js
@@ -1,23 +1,23 @@
 export default [
   {
     albumDescription: '',
-    cover: 'https://pp.userapi.com/c840626/v840626218/25f7c/qQBRJfkKRrU.jpg',
-    photos: ['https://pp.userapi.com/c840626/v840626218/25f7c/qQBRJfkKRrU.jpg', 'https://pp.userapi.com/c840626/v840626218/25f90/NCWnd0NmS_c.jpg', 'https://pp.userapi.com/c840626/v840626218/25f9a/rubbZM80Zvg.jpg']
+    cover: 'https://pp.userapi.com/c840533/v840533086/28654/FduYBPB0X_g.jpg',
+    photos: ['https://pp.userapi.com/c840533/v840533086/2864a/0r0-JOcSRoI.jpg', 'https://pp.userapi.com/c840533/v840533086/2865e/w-pOeHz08P0.jpg', 'https://pp.userapi.com/c840533/v840533086/28668/OAMdXWc-52M.jpg', 'https://pp.userapi.com/c840533/v840533086/28672/7wpmgL9AOBk.jpg']
   }, {
     albumDescription: '',
-    cover: 'https://pp.userapi.com/c840626/v840626218/26012/eplaBp1T_PI.jpg',
-    photos: ['https://pp.userapi.com/c840626/v840626218/2601c/3kuLCD8eM6A.jpg', 'https://pp.userapi.com/c840626/v840626218/26030/xbcZBIkZ278.jpg', 'https://pp.userapi.com/c840626/v840626218/2604e/slcHwmoqgFU.jpg']
+    cover: 'https://pp.userapi.com/c840533/v840533086/286c6/Q900R8nyIVI.jpg',
+    photos: ['https://pp.userapi.com/c840533/v840533086/2867c/86qK-78Fov0.jpg', 'https://pp.userapi.com/c840533/v840533086/2868a/6mzuY1xx7I0.jpg', 'https://pp.userapi.com/c840533/v840533086/28694/M-ywzDudJWo.jpg', 'https://pp.userapi.com/c840533/v840533086/2869e/6eqj75iEDG0.jpg', 'https://pp.userapi.com/c840533/v840533086/286a8/8zevpGXuBmo.jpg', 'https://pp.userapi.com/c840533/v840533086/286b2/aa-FrAO8G-s.jpg', 'https://pp.userapi.com/c840533/v840533086/286bc/N2BlyrZj4BE.jpg']
   }, {
     albumDescription: '',
-    cover: 'https://pp.userapi.com/c840626/v840626218/25fc2/_kUXAJ5g6to.jpg',
-    photos: ['https://pp.userapi.com/c840626/v840626218/26076/ELxSBkLtkbM.jpg','https://pp.userapi.com/c840626/v840626218/25fcc/i6952i2iKPk.jpg', 'https://pp.userapi.com/c840626/v840626218/25fe0/IPFKfi8ccU4.jpg', 'https://pp.userapi.com/c840626/v840626218/25ffe/Hp11m7UV_mY.jpg']
+    cover: 'https://pp.userapi.com/c840533/v840533086/286da/t5TjD3yOjwI.jpg',
+    photos: ['https://pp.userapi.com/c840533/v840533086/286d0/mm2LZbW-g8c.jpg','https://pp.userapi.com/c840533/v840533086/286e4/3KCok8Tn560.jpg']
   }, {
     albumDescription: '',
-    cover: 'https://pp.userapi.com/c840230/v840230307/449f3/-BvQ3dEnD7E.jpg',
-    photos: ['https://pp.userapi.com/c840230/v840230307/449df/Bog1f5ydRVU.jpg', 'https://pp.userapi.com/c840230/v840230307/449fd/hf7kEyMffVk.jpg', 'https://pp.userapi.com/c840230/v840230307/449e9/ixX2Lx2hL3o.jpg']
+    cover: 'https://pp.userapi.com/c840131/v840131282/4bd85/VLpmzg6CRGE.jpg',
+    photos: ['https://pp.userapi.com/c840533/v840533086/28716/j-dMgdMGLls.jpg', 'https://pp.userapi.com/c840533/v840533086/2872e/-d9ps-Z7_ns.jpg', 'https://pp.userapi.com/c841537/v841537086/4279f/S7ZpaU_hCcI.jpg']
   }, {
     albumDescription: '',
-    cover: 'https://pp.userapi.com/c840626/v840626218/26012/eplaBp1T_PI.jpg',
-    photos: ['https://pp.userapi.com/c840626/v840626218/2601c/3kuLCD8eM6A.jpg', 'https://pp.userapi.com/c840626/v840626218/26030/xbcZBIkZ278.jpg', 'https://pp.userapi.com/c840626/v840626218/2604e/slcHwmoqgFU.jpg']
+    cover: 'https://pp.userapi.com/c840533/v840533086/2869e/6eqj75iEDG0.jpg',
+    photos: []
   }
 ];
